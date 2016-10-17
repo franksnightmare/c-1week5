b2874ca8feda72b6dbcc8d8174fcffcf536882a8
Jaime Betancor <ajos.chungos@gmail.com>
2016-10-17 16:33:57 +0200

    Upload exercise 42

--- report/src/a42/strings/at1.cc
+++
@@ -1,9 +0,0 @@
-#include "strings.ih"
-
-string Strings::at(size_t index)
-{
-	if (index >= 0 && index < d_size)
-		return d_str[index];
-	string newString = "null";
-	return newString;
-}
--- report/src/a42/strings/at2.cc
+++
@@ -1,8 +0,0 @@
-#include "strings.ih"
-
-string const Strings::at(size_t index) const
-{
-	if (index >= 0 && index < d_size)
-		return d_str[index];
-	return "null";
-}
